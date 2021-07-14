88ad24b603c37708a8366ccde9143c6a39949af9
Alejandro Hernández <info@alejandrohdezma.com>
2021-07-14 07:30:54 +0200

    Move everything inside `run` function as suggested by GitHub
    Signed-off-by: Alejandro Hernández <info@alejandrohdezma.com>

--- index.js
+++ index.js
@@ -1,71 +1,70 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-// check if this is running on a pull request
-if (!github.context.payload.pull_request) {
-  return core.setOutput('passed', true)
-}
+async function run() {
+  // check if this is running on a pull request
+  if (!github.context.payload.pull_request) {
+    return core.setOutput('passed', true)
+  }
 
-const token = core.getInput('githubToken');
-const context = github.context
-const octokit = github.getOctokit(token)
+  const token = core.getInput('githubToken');
+  const context = github.context
+  const octokit = github.getOctokit(token)
 
-const hasSomeInput = core.getInput('hasSome')
-const hasAllInput = core.getInput('hasAll')
-const hasNoneInput = core.getInput('hasNone')
-const hasNotAllInput = core.getInput('hasNotAll')
+  const hasSomeInput = core.getInput('hasSome')
+  const hasAllInput = core.getInput('hasAll')
+  const hasNoneInput = core.getInput('hasNone')
+  const hasNotAllInput = core.getInput('hasNotAll')
 
-const hasSomeLabels = hasSomeInput.split(',')
-const hasAllLabels = hasAllInput.split(',')
-const hasNoneLabels = hasNoneInput.split(',')
-const hasNotAllLabels = hasNotAllInput.split(',')
+  const hasSomeLabels = hasSomeInput.split(',')
+  const hasAllLabels = hasAllInput.split(',')
+  const hasNoneLabels = hasNoneInput.split(',')
+  const hasNotAllLabels = hasNotAllInput.split(',')
 
-const failMessages = []
+  const failMessages = []
 
+  const prLabels = context.payload.pull_request.labels.map(item => item.name)
 
-const prLabels = context.payload.pull_request.labels.map(item => item.name)
+  const hasSomeResult = !hasSomeInput || hasSomeLabels.some((label) =>
+    prLabels.includes(label)
+  )
 
-const hasSomeResult = !hasSomeInput || hasSomeLabels.some((label) =>
-  prLabels.includes(label)
-)
+  const hasAllResult = !hasAllInput || hasAllLabels.every((label) =>
+    prLabels.includes(label)
+  )
 
-const hasAllResult = !hasAllInput || hasAllLabels.every((label) =>
-  prLabels.includes(label)
-)
+  const hasNoneResult = !hasNoneInput || hasNoneLabels.every((label) =>
+    !prLabels.includes(label)
+  )
 
-const hasNoneResult = !hasNoneInput || hasNoneLabels.every((label) =>
-  !prLabels.includes(label)
-)
+  const hasNotAllResult = !hasNotAllInput || hasNotAllLabels.some((label) =>
+    !prLabels.includes(label)
+  )
 
-const hasNotAllResult = !hasNotAllInput || hasNotAllLabels.some((label) =>
-  !prLabels.includes(label)
-)
+  if (!hasSomeResult) {
+    failMessages.push(`The PR needs to have at least one of the following labels to pass this check: ${hasSomeLabels.join(
+      ', '
+    )}`)
+  }
 
-if (!hasSomeResult) {
-  failMessages.push(`The PR needs to have at least one of the following labels to pass this check: ${hasSomeLabels.join(
-    ', '
-  )}`)
-}
+  if (!hasAllResult) {
+    failMessages.push(`The PR needs to have all of the following labels to pass this check: ${hasAllLabels.join(
+      ', '
+    )}`)
+  }
 
-if (!hasAllResult) {
-  failMessages.push(`The PR needs to have all of the following labels to pass this check: ${hasAllLabels.join(
-    ', '
-  )}`)
-}
+  if (!hasNoneResult) {
+    failMessages.push(`The PR needs to have none of the following labels to pass this check: ${hasNoneLabels.join(
+      ', '
+    )}`)
+  }
 
-if (!hasNoneResult) {
-  failMessages.push(`The PR needs to have none of the following labels to pass this check: ${hasNoneLabels.join(
-    ', '
-  )}`)
-}
-
-if (!hasNotAllResult) {
-  failMessages.push(`The PR needs to not have at least one of the following labels to pass this check: ${hasNotAllLabels.join(
-    ', '
-  )}`)
-}
-
-async function run () {
+  if (!hasNotAllResult) {
+    failMessages.push(`The PR needs to not have at least one of the following labels to pass this check: ${hasNotAllLabels.join(
+      ', '
+    )}`)
+  }
+  
   const checks = await octokit.checks.listForRef({
     ...context.repo,
     ref: context.payload.pull_request.head.ref,
