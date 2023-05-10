7cefb19ea28b2c0bb12a01efb7d42282c5dc095c
Daniel Chabr <daniel.c@alicetechnologies.com>
2023-05-10 13:31:35 +0200

    fix utils path

--- index.cjs
+++ index.cjs
@@ -1,75 +1,81 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
-const { parseInputTags } = require('./utils')
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { parseInputTags } = require("./utils.cjs");
 
 async function run() {
   try {
     // check if this is running on a pull request
     if (!github.context.payload.pull_request) {
-      return core.setOutput('passed', true)
+      return core.setOutput("passed", true);
     }
 
-    const token = core.getInput('githubToken');
-    const context = github.context
-    const octokit = github.getOctokit(token)
+    const token = core.getInput("githubToken");
+    const context = github.context;
+    const octokit = github.getOctokit(token);
 
-    const hasSomeInput = core.getInput('hasSome')
-    const hasAllInput = core.getInput('hasAll')
-    const hasNoneInput = core.getInput('hasNone')
-    const hasNotAllInput = core.getInput('hasNotAll')
+    const hasSomeInput = core.getInput("hasSome");
+    const hasAllInput = core.getInput("hasAll");
+    const hasNoneInput = core.getInput("hasNone");
+    const hasNotAllInput = core.getInput("hasNotAll");
 
-    const hasSomeLabels = parseInputTags(hasSomeInput)
-    const hasAllLabels = parseInputTags(hasAllInput)
-    const hasNoneLabels = parseInputTags(hasNoneInput)
-    const hasNotAllLabels = parseInputTags(hasNotAllInput)
+    const hasSomeLabels = parseInputTags(hasSomeInput);
+    const hasAllLabels = parseInputTags(hasAllInput);
+    const hasNoneLabels = parseInputTags(hasNoneInput);
+    const hasNotAllLabels = parseInputTags(hasNotAllInput);
 
-    const failMessages = []
+    const failMessages = [];
 
     const { data: labelsOnIssue } = await octokit.issues.listLabelsOnIssue({
       ...context.repo,
-      issue_number: context.payload.pull_request.number
-    })
+      issue_number: context.payload.pull_request.number,
+    });
 
-    const prLabels = labelsOnIssue.map(item => item.name)
+    const prLabels = labelsOnIssue.map((item) => item.name);
 
-    const hasSomeResult = !hasSomeInput || hasSomeLabels.some((label) =>
-      prLabels.includes(label)
-    )
+    const hasSomeResult =
+      !hasSomeInput || hasSomeLabels.some((label) => prLabels.includes(label));
 
-    const hasAllResult = !hasAllInput || hasAllLabels.every((label) =>
-      prLabels.includes(label)
-    )
+    const hasAllResult =
+      !hasAllInput || hasAllLabels.every((label) => prLabels.includes(label));
 
-    const hasNoneResult = !hasNoneInput || hasNoneLabels.every((label) =>
-      !prLabels.includes(label)
-    )
+    const hasNoneResult =
+      !hasNoneInput ||
+      hasNoneLabels.every((label) => !prLabels.includes(label));
 
-    const hasNotAllResult = !hasNotAllInput || hasNotAllLabels.some((label) =>
-      !prLabels.includes(label)
-    )
+    const hasNotAllResult =
+      !hasNotAllInput ||
+      hasNotAllLabels.some((label) => !prLabels.includes(label));
 
     if (!hasSomeResult) {
-      failMessages.push(`The PR needs to have at least one of the following labels to pass this check: ${hasSomeLabels.join(
-        ', '
-      )}`)
+      failMessages.push(
+        `The PR needs to have at least one of the following labels to pass this check: ${hasSomeLabels.join(
+          ", "
+        )}`
+      );
     }
 
     if (!hasAllResult) {
-      failMessages.push(`The PR needs to have all of the following labels to pass this check: ${hasAllLabels.join(
-        ', '
-      )}`)
+      failMessages.push(
+        `The PR needs to have all of the following labels to pass this check: ${hasAllLabels.join(
+          ", "
+        )}`
+      );
     }
 
     if (!hasNoneResult) {
-      failMessages.push(`The PR needs to have none of the following labels to pass this check: ${hasNoneLabels.join(
-        ', '
-      )}`)
+      failMessages.push(
+        `The PR needs to have none of the following labels to pass this check: ${hasNoneLabels.join(
+          ", "
+        )}`
+      );
     }
 
     if (!hasNotAllResult) {
-      failMessages.push(`The PR needs to not have at least one of the following labels to pass this check: ${hasNotAllLabels.join(
-        ', '
-      )}`)
+      failMessages.push(
+        `The PR needs to not have at least one of the following labels to pass this check: ${hasNotAllLabels.join(
+          ", "
+        )}`
+      );
     }
 
     const checks = await octokit.checks.listForRef({
@@ -77,7 +83,9 @@ async function run() {
       ref: context.payload.pull_request.head.ref,
     });
 
-    const checkRunIds = checks.data.check_runs.filter(check => check.name === context.job).map(check => check.id)
+    const checkRunIds = checks.data.check_runs
+      .filter((check) => check.name === context.job)
+      .map((check) => check.id);
 
     if (failMessages.length) {
       // update old checks
@@ -85,34 +93,34 @@ async function run() {
         await octokit.checks.update({
           ...context.repo,
           check_run_id: id,
-          conclusion: 'failure',
+          conclusion: "failure",
           output: {
-            title: 'Labels did not pass provided rules',
-            summary: failMessages.join('. ')
-          }
-        })
+            title: "Labels did not pass provided rules",
+            summary: failMessages.join(". "),
+          },
+        });
       }
 
-      core.setFailed(failMessages.join('. '))
+      core.setFailed(failMessages.join(". "));
     } else {
       // update old checks
       for (const id of checkRunIds) {
         await octokit.checks.update({
           ...context.repo,
           check_run_id: id,
-          conclusion: 'success',
+          conclusion: "success",
           output: {
-            title: 'Labels follow all the provided rules',
-            summary: ''
-          }
-        })
+            title: "Labels follow all the provided rules",
+            summary: "",
+          },
+        });
       }
 
-      core.setOutput('passed', true)
+      core.setOutput("passed", true);
     }
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
